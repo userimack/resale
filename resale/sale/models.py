21099bf45b5d01cccd73852cdad3dff4a024d60f
Mahendra Yadav <mahendra@azrisolutions.com>
2017-04-02 22:32:48 +0530

    Added models and views

--- resale/sale/models.py
+++ resale/sale/models.py
@@ -5,9 +5,26 @@ from django.utils import timezone
 
 
 class User(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.IntegerField()
+    password = models.CharField(max_length=20) 
+
+    def __str__(self):
+        return self.name
 
 
+class ticket(models.Model):
+    conference = models.CharField(max_length=255)
+    type_of_ticket = models.CharField(max_length=255)
+    price = models.CharField(max_length=10)
+    CHOICES = (
+              (u'0', u'Sold'),
+              (u'1', u'Unsold'),
+              (u'2', u'Wait')
+    )
+    status = models.CharField(max_length=1, choices=CHOICES)
+    date = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return "%s-%s-%s".format(self.date, self.conference, self.type_of_ticket)
